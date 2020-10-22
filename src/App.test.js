0262045011e39982d8565932a3fe0113e2790991
Nhut Nguyen Minh <nhut.nguyen@salto.vn>
2020-10-22 16:36:54 +0700

    Fix circleCI fail

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,3 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import App from './App'
-
 test('renders learn react link', () => {
   const linkElement = '1'
   expect(linkElement).toBe('1')
